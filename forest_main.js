8b5d9ce2c0a1fcd800eb0fd96d528dd39e07b46d
Matt <labonville@gmail.com>
2016-07-22 11:24:17 -0400

    fixed pin addressing issue

--- forest_main.js
+++ forest_main.js
@@ -7,16 +7,16 @@ board.on("ready", function() {
     var exit_sol = new five.Relay(24);
     var soak_sol = new five.Relay(25);
     var mist_sol = new five.Relay(26);
-    var ac = new five.Relay(26);
-    var fans = new five.Relay(27);
-    var ozzie = new five.Relay(28);
-    var light1 = new five.Relay(29);
-    var light2 = new five.Relay(20);
-    var light3 = new five.Relay(31);
-    var intake = new five.Relay(32);
-    var exhaust = new five.Relay(33);
-    var chiller = new five.Relay(34);
-    var pump = new five.Relay(35);
+    var ac = new five.Relay(27);
+    var fans = new five.Relay(28);
+    var ozzie = new five.Relay(29);
+    var light1 = new five.Relay(30);
+    var light2 = new five.Relay(31);
+    var light3 = new five.Relay(32);
+    var intake = new five.Relay(33);
+    var exhaust = new five.Relay(34);
+    var chiller = new five.Relay(35);
+    var pump = new five.Relay(36);
 
     // From the REPL, toggle the lamp on and off by calling:
     //
