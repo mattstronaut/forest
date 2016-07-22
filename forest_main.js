bf2d29d08017a8b3821268addceeaa95e5e1c1e2
Matt <labonville@gmail.com>
2016-07-22 12:23:45 -0400

    testing issue with github not displaying freshly uploaded files from webstorm

--- forest_main.js
+++ forest_main.js
@@ -4,77 +4,77 @@ var five = require("johnny-five"),
 
 board.on("ready", function() {
     var in_sol = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 22
     });
     in_sol.off();
     var out_sol = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 23
     });
     out_sol.off();
     var exit_sol = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 24
     });
     exit_sol.off();
     var soak_sol = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 25
     });
     soak_sol.off();
     var mist_sol = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 26
     });
     mist_sol.off();
     var ac = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 27
     });
     ac.off();
     var fans = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 28
     });
     fans.off();
     var ozzie = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 29
     });
     ozzie.off();
     var light1 = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 30
     });
     light1.off();
     var light2 = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 31
     });
     light2.off();
     var light3 = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 32
     });
     light3.off();
     var intake = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 33
     });
     intake.off();
     var exhaust = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 34
     });
     exhaust.off();
     var chiller = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 35
     });
     chiller.off();
     var pump = new five.Relay({
-        typ: "NC",
+        type: "NC",
         pin: 36
     });
     pump.off();
