5cb931276dff74fd3f6050e7025ea5e81727276e
Matt <labonville@gmail.com>
2016-07-22 12:20:41 -0400

    testing issue with github not displaying freshly uploaded files from webstorm

--- forest_main.js
+++ forest_main.js
@@ -3,21 +3,81 @@ var five = require("johnny-five"),
     board = new five.Board();
 
 board.on("ready", function() {
-    var in_sol = new five.Relay(22);
-    var out_sol = new five.Relay(23);
-    var exit_sol = new five.Relay(24);
-    var soak_sol = new five.Relay(25);
-    var mist_sol = new five.Relay(26);
-    var ac = new five.Relay(27);
-    var fans = new five.Relay(28);
-    var ozzie = new five.Relay(29);
-    var light1 = new five.Relay(30);
-    var light2 = new five.Relay(31);
-    var light3 = new five.Relay(32);
-    var intake = new five.Relay(33);
-    var exhaust = new five.Relay(34);
-    var chiller = new five.Relay(35);
-    var pump = new five.Relay(36);
+    var in_sol = new five.Relay({
+        typ: "NC",
+        pin: 22
+    });
+    in_sol.off();
+    var out_sol = new five.Relay({
+        typ: "NC",
+        pin: 23
+    });
+    out_sol.off();
+    var exit_sol = new five.Relay({
+        typ: "NC",
+        pin: 24
+    });
+    exit_sol.off();
+    var soak_sol = new five.Relay({
+        typ: "NC",
+        pin: 25
+    });
+    soak_sol.off();
+    var mist_sol = new five.Relay({
+        typ: "NC",
+        pin: 26
+    });
+    mist_sol.off();
+    var ac = new five.Relay({
+        typ: "NC",
+        pin: 27
+    });
+    ac.off();
+    var fans = new five.Relay({
+        typ: "NC",
+        pin: 28
+    });
+    fans.off();
+    var ozzie = new five.Relay({
+        typ: "NC",
+        pin: 29
+    });
+    ozzie.off();
+    var light1 = new five.Relay({
+        typ: "NC",
+        pin: 30
+    });
+    light1.off();
+    var light2 = new five.Relay({
+        typ: "NC",
+        pin: 31
+    });
+    light2.off();
+    var light3 = new five.Relay({
+        typ: "NC",
+        pin: 32
+    });
+    light3.off();
+    var intake = new five.Relay({
+        typ: "NC",
+        pin: 33
+    });
+    intake.off();
+    var exhaust = new five.Relay({
+        typ: "NC",
+        pin: 34
+    });
+    exhaust.off();
+    var chiller = new five.Relay({
+        typ: "NC",
+        pin: 35
+    });
+    chiller.off();
+    var pump = new five.Relay({
+        typ: "NC",
+        pin: 36
+    });
+    pump.off();
 
     // From the REPL, toggle the lamp on and off by calling:
     //
